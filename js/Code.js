ba3ed9cf1ba1da7f1a4f431f596d717a2b6be7c2
RamyaVatsa <76208457+RamyaVatsa@users.noreply.github.com>
2021-07-09 18:05:47 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("N I O N C T F U", 700,160) 
+fill("lightblue")
+text("Hint: Draw , Setup , Preload !!", 700,180)
+
+fill("white")
+textSize(15)
+text("S A B E T A A D ", 100,300) 
+fill("lightblue")
+text("Hint: Car Racing Game !!", 100,320)
+
+}
